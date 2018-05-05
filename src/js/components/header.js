f0cd184ff5b24ceaf9d67a8113799af1112dd627
Arka Roy <i.arkaroy@gmail.com>
2018-05-05 12:54:41 +0530

    Updated menu

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -3,15 +3,32 @@ import { Link } from 'react-router-dom';
 
 class Header extends Component {
 
+    constructor(props) {
+        super(props);
+        this.handleBarClick = this.handleBarClick.bind(this);
+        this.state = {
+            showMenu: false
+        };
+    }
+
+    handleBarClick() {
+        const showMenu = this.state.showMenu;
+        this.setState({
+            showMenu: !showMenu
+        });
+    }
+
     render() {
+        const showMenu = this.state.showMenu;
         return (
             <header className="header">
-                <h1>ARKA ROY</h1>
-                <div className="bar"><div></div></div>
-                <ul className="menu">
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/projects">Projects</Link></li>
-                </ul>
+                <div className={showMenu ? 'bar closed' : 'bar'} ref="bar" onClick={this.handleBarClick}><div></div></div>
+                <div className={showMenu ? 'menu open' : 'menu'}>
+                    <ul>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/projects">Projects</Link></li>
+                    </ul>
+                </div>
             </header>
         );
     }
