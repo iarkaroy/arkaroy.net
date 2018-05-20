8d17f07baaeef858791a2a5ed1d70a6bf04cf3af
Arka Roy <i.arkaroy@gmail.com>
2018-05-20 20:42:16 +0530

    Updated header with shape overlay

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -1,34 +1,44 @@
 import React, { Component } from 'react';
 import { Link } from '../router/router';
+import ShapeOverlays from '../shape-overlays';
 
 class Header extends Component {
 
     constructor(props) {
         super(props);
-        this.handleBarClick = this.handleBarClick.bind(this);
         this.state = {
-            showMenu: false
+            menuOpened: false
         };
     }
 
-    handleBarClick() {
-        const showMenu = this.state.showMenu;
-        this.setState({
-            showMenu: !showMenu
-        });
+    componentDidMount() {
+        this.overlays = document.querySelector('.shape-overlays');
+        this.overlay = new ShapeOverlays(this.overlays);
     }
 
+    handleBarClick = (event) => {
+        if (this.overlay.isAnimating)
+            return false;
+        this.overlay.toggle();
+        this.setState({
+            menuOpened: this.overlay.isOpened === true
+        });
+    };
+
     render() {
-        const showMenu = this.state.showMenu;
         return (
             <header className="header">
-                <div className={showMenu ? 'bar closed' : 'bar'} ref="bar" onClick={this.handleBarClick}><div></div></div>
-                <div className={showMenu ? 'menu open' : 'menu'}>
+                <div className={this.state.menuOpened ? 'bar closed' : 'bar'} ref="bar" onClick={this.handleBarClick}><div></div></div>
+                <div className={this.state.menuOpened ? 'menu opened' : 'menu'}>
                     <ul onClick={this.handleBarClick}>
                         <Link to="/">Home</Link>
                         <Link to="/projects">Projects</Link>
                     </ul>
                 </div>
+                <svg className="shape-overlays" viewBox="0 0 100 100" preserveAspectRatio="none">
+                    <path className="shape-overlays__path"></path>
+                    <path className="shape-overlays__path"></path>
+                </svg>
             </header>
         );
     }
