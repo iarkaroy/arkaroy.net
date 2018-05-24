a7aa2751b4598e3c37979200b8f9c058e9fffb77
Arka Roy <i.arkaroy@gmail.com>
2018-05-24 23:28:46 +0530

    Updated intro with rotating slices in webgl

--- src/js/components/intro.js
+++ src/js/components/intro.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import FontFaceObserver from 'fontfaceobserver';
 import * as glUtils from '../webgl-utils';
 
-const vs = `
+const vsQuad = `
 #ifdef GL_ES
 precision highp float;
 #endif
@@ -14,7 +14,7 @@ void main() {
 }
 `;
 
-const fs = `
+const fsSimulation = `
 #ifdef GL_ES
 precision highp float;
 #endif
@@ -22,12 +22,12 @@ precision highp float;
 uniform sampler2D u_texture;
 uniform vec2 u_resolution;
 uniform float u_angle;
-uniform float u_offsets[30];
-uniform vec2 u_points[30];
+uniform float u_offsets[40];
+uniform vec2 u_points[40];
 uniform vec2 u_v1;
 uniform vec2 u_v2;
 
-const int TOTAL = 30;
+const int TOTAL = 40;
 
 void main() {
     vec2 div = 1.0 / u_resolution;
@@ -53,30 +53,19 @@ void main() {
 }
 `;
 
-const testVS = `
+const fsRender = `
 #ifdef GL_ES
 precision highp float;
 #endif
 
-attribute vec2 a_position;
+uniform sampler2D u_texture;
 uniform vec2 u_resolution;
 
 void main() {
-    vec2 pos = a_position / u_resolution;
-    pos *= 2.0;
-    pos -= 1.0;
-    gl_Position = vec4(pos,0,1);
-    gl_PointSize = 4.0;
-}
-`;
-
-const testFS = `
-#ifdef GL_ES
-precision highp float;
-#endif
-
-void main() {
-    gl_FragColor = vec4(1);
+    vec2 div = 1.0 / u_resolution.xy;
+    vec2 uv = gl_FragCoord.xy * div;
+    uv.y = 1.0 - uv.y;
+    gl_FragColor = texture2D( u_texture, uv );
 }
 `;
 
@@ -98,13 +87,15 @@ class Intro extends Component {
         };
         this.angle = 20;
         this.size = 0;
-        this.divs = 30;
+        this.divs = 40;
         this.offsets = new Float32Array([]);
         this.points = new Float32Array([]);
         this.gl = null;
-        this.program = null;
+        this.simulationProgram = null;
+        this.renderProgram = null;
         this.buffer = null;
         this.texture = null;
+        this.fbo = null;
         this.frameId = 0;
     }
 
@@ -114,46 +105,57 @@ class Intro extends Component {
                 weight: 900
             });
             font.load().then(this.generateImage, this.generateImage);
+            this.initWebGL();
         });
-        this.initWebGL();
     }
 
     loop = () => {
+        var t0 = performance.now();
+        
         this.frameId = requestAnimationFrame(this.loop);
 
         const { width, height } = this.state.viewport;
 
-        this.gl.useProgram(this.program);
-        this.texture.bind(0, this.program.u_texture);
-        this.buffer.data(QUAD, this.program.a_position, 2);
-        this.gl.uniform2fv(this.program.u_resolution, new Float32Array([width, height]));
-        this.gl.uniform1f(this.program.u_angle, this.angle * Math.PI / 180);
-        this.gl.uniform1fv(this.program.u_offsets, this.offsets);
-        this.gl.uniform2fv(this.program.u_points, this.points);
-        this.gl.uniform2fv(this.program.u_v1, this.v1);
-        this.gl.uniform2fv(this.program.u_v2, this.v2);
+        var program = this.simulationProgram;
+        this.gl.useProgram(program);
+        this.texture.bind(0, program.u_texture);
+        this.buffer.data(QUAD, program.a_position, 2);
+        this.gl.uniform2fv(program.u_resolution, new Float32Array([width, height]));
+        this.gl.uniform1f(program.u_angle, this.angle * Math.PI / 180);
+        this.gl.uniform1fv(program.u_offsets, this.offsets);
+        this.gl.uniform2fv(program.u_points, this.points);
+        this.gl.uniform2fv(program.u_v1, this.v1);
+        this.gl.uniform2fv(program.u_v2, this.v2);
+        this.fbo.bind();
         glUtils.reset(this.gl, width, height, true);
         this.gl.drawArrays(this.gl.TRIANGLE_STRIP, 0, QUAD.length / 2);
-        this.updateOffsets();
-        /*
-        this.gl.useProgram(this.testProgram);
-        this.buffer.data(this.points, this.testProgram.a_position, 2);
-        this.gl.uniform2fv(this.testProgram.u_resolution, new Float32Array([width, height]));
-        this.gl.drawArrays(this.gl.POINTS, 0, this.points.length / 2);        
-        */
+        this.angle += 0.5;
+        this.generateBoundingBox();
+
+        program = this.renderProgram;
+        this.gl.useProgram(program);
+        this.fbo.texture0.bind(0, program.u_texture);
+        this.buffer.data(QUAD, program.a_position, 2);
+        this.gl.uniform2fv(program.u_resolution, new Float32Array([width, height]));
+        this.fbo.unbind();
+        glUtils.reset(this.gl, width, height, true);
+        this.gl.drawArrays(this.gl.TRIANGLE_STRIP, 0, QUAD.length / 2);
+        
+        var t1 = performance.now();
     };
 
     initWebGL = () => {
+        const { width, height } = this.state.viewport;
         this.gl = glUtils.getWebGLContext(this.canvas);
-        this.program = glUtils.program(this.gl, vs, fs);
+        this.simulationProgram = glUtils.program(this.gl, vsQuad, fsSimulation);
+        this.renderProgram = glUtils.program(this.gl, vsQuad, fsRender);
         this.buffer = glUtils.buffer(this.gl);
-
-        // this.testProgram = glUtils.program(this.gl, testVS, testFS);
+        this.fbo = glUtils.framebuffer(this.gl, width, height);
     };
 
     generateImage = () => {
-        const text = 'DEMO';
-        const font = '900 100px Barlow, sans-serif';
+        const text = 'DEMO TEXT';
+        const font = '900 280px Barlow, sans-serif';
         const { width, height } = this.state.viewport;
         const ctx = document.createElement('canvas').getContext('2d');
         ctx.font = font;
@@ -229,41 +231,34 @@ class Intro extends Component {
     generateOffsets = () => {
         var offsets = [];
         for (let i = 1; i <= this.divs; ++i) {
-            offsets.push(Math.random() * 100);
+            const o = Math.random() * 10 + 10;
+            offsets.push(Math.random() < 0.5 ? o : -o);
+            // offsets.push(0);
         }
         this.offsets = new Float32Array(offsets);
     };
-
-    updateOffsets = () => {
-        var offsets = [];
-        for (let i = 0; i < this.divs; ++i) {
-            offsets.push(this.offsets[i] * 0.96);
-        }
-        this.offsets = new Float32Array(offsets);
-        //console.log(this.offsets);
-    }
 
     updateViewport = (callback = null) => {
         const { innerWidth, innerHeight } = window;
         this.setState({
             viewport: {
-                width: innerWidth,
-                height: innerHeight,
+                width: innerWidth * 2,
+                height: innerHeight * 2,
                 center: {
-                    x: innerWidth / 2,
-                    y: innerHeight / 2
+                    x: innerWidth,
+                    y: innerHeight
                 }
             }
         }, callback);
     };
 
     render() {
-        const viewport = this.state.viewport;
+        const { width, height, center } = this.state.viewport;
         return (
             <canvas ref={o => { this.canvas = o }}
-                width={viewport.width}
-                height={viewport.height}
-                style={{ zIndex: 2 }}
+                width={width}
+                height={height}
+                style={{ zIndex: 2, width: center.x, height: center.y }}
             />
         );
     }
