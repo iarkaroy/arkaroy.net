734273d229ea5e3be34b3b2f636054f92c3e4f62
Arka Roy <i.arkaroy@gmail.com>
2018-06-03 23:14:05 +0530

    Implemented liquid effect in intro

--- src/js/components/intro.js
+++ src/js/components/intro.js
@@ -12,48 +12,236 @@ class Intro extends Component {
             }
         };
         this.ctx = null;
+        this.chars = [];
+        this.totalWidth = 0;
+        this.letterSpacing = 25;
+        this.smoothing = 0.3;
+        this.scale = 1;
+        this.frameId = 0;
+        this.timeoutId = 0;
+
+        this.mouse = {
+            position: { x: 0, y: 0 },
+            direction: { x: 0, y: 0 },
+            speed: { x: 0, y: 0 },
+            last: { x: 0, y: 0 },
+            dist: 60
+        };
+        this.viscosity = 20;
+        this.damping = 0.05;
     }
 
     componentDidMount() {
-        this.updateViewport(() => {
-            this.ctx = this.canvas.getContext('2d');
-            this.ctx.globalCompositeOperation = 'xor';
-            var totalWidth = 0;
-            var diff = 25;
-            characters.forEach((char, i) => {
-                totalWidth += char.width;
-                if (i < characters.length - 1)
-                    totalWidth += diff;
-            });
-            var scale = this.state.viewport.width / totalWidth;
-            console.log(scale);
-            var offset = 0;
-            characters.forEach(char => {
-                if (char.outer.length > 0) {
-                    this.ctx.beginPath();
-                    this.ctx.moveTo(char.outer[0].p.x * scale + offset, char.outer[0].p.y * scale);
-                    char.outer.forEach((c, i) => {
-                        if (i === 0) return false;
-                        this.ctx.lineTo(c.p.x * scale + offset, c.p.y * scale);
-                    });
-                    this.ctx.fillStyle = '#fff';
-                    this.ctx.fill();
-                }
-                if (char.inner.length > 0) {
-                    this.ctx.beginPath();
-                    this.ctx.moveTo(char.inner[0].p.x * scale + offset, char.inner[0].p.y * scale);
-                    char.inner.forEach((c, i) => {
-                        if (i === 0) return false;
-                        this.ctx.lineTo(c.p.x * scale + offset, c.p.y * scale);
-                    });
-                    this.ctx.fillStyle = '#fff';
-                    this.ctx.fill();
-                }
-                offset += char.width * scale + 25 * scale;
-            });
-        });
+        window.addEventListener('resize', this.onResize);
+        document.addEventListener('mousemove', this.onMouseMove);
+        this.setup();
+        this.mouseSpeed();
     }
 
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.onResize);
+        document.removeEventListener('mousemove', this.onMouseMove);
+        if (this.frameId) {
+            cancelAnimationFrame(this.frameId);
+            this.frameId = 0;
+        }
+        if (this.timeoutId) {
+            clearTimeout(this.timeoutId);
+            this.timeoutId = 0;
+        }
+    }
+
+    onResize = (event) => {
+        this.setup();
+    };
+
+    onMouseMove = (event) => {
+        var { pageX, pageY } = event;
+        pageX *= 2;
+        pageY *= 2;
+        if (this.mouse.position.x < pageX)
+            this.mouse.direction.x = 1;
+        else if (this.mouse.position.x > pageX)
+            this.mouse.direction.x = -1;
+        else
+            this.mouse.direction.x = 0;
+
+        if (this.mouse.position.y < pageY)
+            this.mouse.direction.y = 1;
+        else if (this.mouse.position.y > pageY)
+            this.mouse.direction.y = -1;
+        else
+            this.mouse.direction.y = 0;
+
+        this.mouse.position.x = pageX;
+        this.mouse.position.y = pageY;
+    };
+
+    mouseSpeed = () => {
+        this.mouse.speed.x = this.mouse.position.x - this.mouse.last.x;
+        this.mouse.speed.y = this.mouse.position.y - this.mouse.last.y;
+
+        this.mouse.last.x = this.mouse.position.x;
+        this.mouse.last.y = this.mouse.position.y;
+        this.timeoutId = setTimeout(this.mouseSpeed, 20);
+    };
+
+    setup = () => {
+        this.updateViewport(() => {
+            const { width, height } = this.state.viewport;
+            if (this.ctx === null)
+                this.ctx = this.canvas.getContext('2d');
+
+            if (this.totalWidth === 0)
+                this.calcTotalWidth();
+
+            this.scale = width * 0.8 / this.totalWidth;
+            if(this.scale > 0.8) this.scale = 0.8;
+
+            this.resetChars();
+
+            if (!this.frameId)
+                this.frameId = requestAnimationFrame(this.loop);
+        });
+    };
+
+    loop = () => {
+        this.frameId = requestAnimationFrame(this.loop);
+        const { width, height } = this.state.viewport;
+        this.ctx.clearRect(0, 0, width, height);
+        this.ctx.save();
+        this.ctx.globalCompositeOperation = 'xor';
+        this.chars.forEach(char => {
+            char.outer.forEach(this.movePoint);
+            char.inner.forEach(this.movePoint);
+            this.calcCtrlPoints(char.outer);
+            this.calcCtrlPoints(char.inner);
+            this.drawPoints(char.outer);
+            this.drawPoints(char.inner);
+        });
+        this.ctx.restore();
+    };
+
+    movePoint = point => {
+        if (point.d > 0) {
+            point.d--;
+            return;
+        }
+        point.v.x += (point.o.x - point.p.x) / this.viscosity;
+        point.v.y += (point.o.y - point.p.y) / this.viscosity;
+
+        var dx = point.o.x - this.mouse.position.x,
+            dy = point.o.y - this.mouse.position.y;
+        var relDist = (1 - Math.sqrt((dx * dx) + (dy * dy)) / this.mouse.dist);
+
+        // Move x
+        if ((this.mouse.direction.x > 0 && this.mouse.position.x > point.p.x)
+            || (this.mouse.direction.x < 0 && this.mouse.position.x < point.p.x)) {
+            if (relDist > 0 && relDist < 1) {
+                point.v.x = (this.mouse.speed.x / 4) * relDist;
+            }
+        }
+        point.v.x *= (1 - this.damping);
+        point.p.x += point.v.x;
+
+        // Move y
+        if ((this.mouse.direction.y > 0 && this.mouse.position.y > point.p.y)
+            || (this.mouse.direction.y < 0 && this.mouse.position.y < point.p.y)) {
+            if (relDist > 0 && relDist < 1) {
+                point.v.y = (this.mouse.speed.y / 4) * relDist;
+            }
+        }
+        point.v.y *= (1 - this.damping);
+        point.p.y += point.v.y;
+    };
+
+    calcCtrlPoints = points => {
+        const len = points.length;
+        for (let i = 0; i < len; ++i) {
+
+            const prev = points[(i + len - 1) % len];
+            const point = points[i];
+            const next = points[(i + len + 1) % len];
+            const dPrev = this.distance(point, prev);
+            const dNext = this.distance(point, next);
+
+            const line = {
+                x: next.p.x - prev.p.x,
+                y: next.p.y - prev.p.y,
+            };
+            const dLine = Math.sqrt(line.x * line.x + line.y * line.y);
+
+            point.cPrev = {
+                x: point.p.x - (line.x / dLine) * dPrev * this.smoothing,
+                y: point.p.y - (line.y / dLine) * dPrev * this.smoothing,
+            };
+            point.cNext = {
+                x: point.p.x + (line.x / dLine) * dNext * this.smoothing,
+                y: point.p.y + (line.y / dLine) * dNext * this.smoothing,
+            };
+
+        }
+    };
+
+    drawPoints = (points, color = '#f0f0f0') => {
+        if (points.length < 1) return;
+        this.ctx.beginPath();
+        this.ctx.moveTo(points[0].p.x, points[0].p.y);
+        for (let p = 1; p < points.length; ++p) {
+            const cnx = points[(p + 0) % points.length].cNext.x;
+            const cny = points[(p + 0) % points.length].cNext.y;
+            const cpx = points[(p + 1) % points.length].cPrev.x;
+            const cpy = points[(p + 1) % points.length].cPrev.y;
+            const px = points[(p + 1) % points.length].p.x;
+            const py = points[(p + 1) % points.length].p.y;
+            this.ctx.bezierCurveTo(cnx, cny, cpx, cpy, px, py);
+        }
+        this.ctx.fillStyle = color;
+        this.ctx.fill();
+    };
+
+    distance = (p1, p2) => {
+        return Math.sqrt(Math.pow(p1.p.x - p2.p.x, 2) + Math.pow(p1.p.y - p2.p.y, 2));
+    };
+
+    resetChars = () => {
+        const { width, height } = this.state.viewport;
+        var offsetX = (width - this.totalWidth * this.scale) / 2;
+        var offsetY = 0;
+        this.chars = [];
+        characters.forEach(ch => {
+            var char = {
+                outer: [],
+                inner: []
+            };
+            offsetY = (height - ch.height * this.scale) / 2;
+            ch.outer.forEach(co => {
+                char.outer.push(this.createPoint(
+                    co.x * this.scale + offsetX,
+                    co.y * this.scale + offsetY
+                ));
+            });
+            ch.inner.forEach(ci => {
+                char.inner.push(this.createPoint(
+                    ci.x * this.scale + offsetX,
+                    ci.y * this.scale + offsetY
+                ));
+            });
+            this.chars.push(char);
+            offsetX += (ch.width + this.letterSpacing) * this.scale;
+        });
+    };
+
+    calcTotalWidth = () => {
+        this.totalWidth = 0;
+        characters.forEach((char, i) => {
+            this.totalWidth += char.width;
+            if (i < characters.length - 1)
+                this.totalWidth += this.letterSpacing;
+        });
+    };
+
+    /*
     createCharacter = () => {
         var outerPoints = [[548, 253], [548, 229], [548, 203], [552, 177], [560, 153], [574, 130], [591, 112], [614, 98], [637, 89], [664, 84], [691, 83], [718, 87], [744, 95], [764, 105], [784, 122], [801, 145], [810, 165], [816, 194], [817, 221], [817, 246], [817, 272], [817, 300], [815, 328], [808, 352], [797, 373], [783, 391], [763, 407], [741, 418], [716, 426], [692, 428], [664, 428], [637, 423], [613, 413], [590, 398], [573, 380], [561, 361], [552, 336], [548, 305], [548, 279]];
         var innerPoints = [[638, 257], [638, 235], [639, 210], [644, 184], [661, 166], [685, 161], [709, 169], [722, 186], [726, 211], [727, 233], [727, 258], [727, 283], [726, 309], [719, 333], [703, 347], [685, 351], [662, 347], [646, 332], [640, 311], [638, 285]];
@@ -88,29 +276,31 @@ class Intro extends Component {
         })
         console.log(char);
     };
-
-    createPoint = point => {
+*/
+    createPoint = (x, y) => {
+        const { width, height } = this.state.viewport;
         return {
             p: {
-                x: point[0],
-                y: point[1]
+                x: x,
+                y: 0
             },
             o: {
-                x: point[0],
-                y: point[1]
+                x,
+                y
             },
             v: {
                 x: 0,
                 y: 0
-            }
+            },
+            d: Math.floor(Math.random() * 5)
         };
     };
 
     updateViewport(callback) {
         this.setState({
             viewport: {
-                width: window.innerWidth,
-                height: window.innerHeight
+                width: window.innerWidth * 2,
+                height: window.innerHeight * 2
             }
         }, callback);
     }
@@ -122,7 +312,7 @@ class Intro extends Component {
                 ref={o => { this.canvas = o; }}
                 width={width}
                 height={height}
-                style={{ zIndex: 2 }}
+                style={{ zIndex: 2, width: width / 2, height: height / 2 }}
             />
         );
     }
@@ -130,31 +320,3 @@ class Intro extends Component {
 }
 
 export default Intro;
-
-/*
-
-A outer
-[551, 324],[561, 293],[572, 258],[583, 222],[594, 187],[605, 151],[615, 120],[623, 96],[633, 88],[661, 87],[695, 87],[731, 88],[741, 95],[750, 121],[759, 148],[767, 175],[778, 208],[789, 245],[800, 278],[810, 310],[821, 345],[832, 380],[841, 414],[838, 422],[812, 423],[783, 423],[757, 423],[747, 418],[741, 401],[737, 382],[731, 377],[702, 376],[670, 376],[636, 377],[628, 383],[623, 402],[618, 418],[608, 423],[586, 423],[559, 423],[531, 422],[524, 415],[533, 382],[541, 354]
-
-A inner
-[664, 250],[672, 221],[679, 198],[685, 198],[693, 222],[701, 251],[708, 277],[714, 301],[712, 308],[687, 309],[657, 308],[651, 303],[656, 280]
-
-R outer
-[554, 357],[554, 330],[554, 302],[554, 273],[554, 240],[554, 205],[554, 171],[554, 133],[555, 94],[561, 88],[596, 87],[630, 87],[665, 87],[692, 87],[722, 88],[752, 95],[777, 109],[796, 128],[809, 152],[815, 180],[814, 211],[807, 237],[790, 262],[763, 282],[758, 291],[769, 315],[782, 342],[794, 368],[805, 391],[817, 414],[813, 422],[790, 424],[761, 424],[735, 423],[722, 417],[712, 395],[701, 370],[692, 349],[681, 324],[671, 300],[666, 294],[650, 294],[645, 298],[644, 321],[644, 348],[644, 381],[644, 417],[636, 423],[614, 424],[588, 424],[565, 423],[555, 419],[554, 385]
-
-R inner
-[676, 225],[654, 225],[644, 220],[643, 196],[645, 169],[650, 164],[674, 163],[697, 165],[717, 175],[725, 196],[717, 216],[700, 224]
-
-K outer
-[506, 372],[506, 344],[506, 316],[506, 286],[506, 254],[506, 225],[506, 192],[506, 163],[507, 128],[512, 121],[541, 120],[564, 120],[591, 121],[596, 128],[596, 157],[596, 181],[596, 208],[596, 235],[604, 235],[621, 211],[637, 188],[653, 166],[667, 146],[684, 125],[695, 121],[720, 120],[748, 120],[779, 121],[782, 126],[767, 149],[754, 167],[739, 188],[724, 208],[704, 236],[685, 263],[685, 276],[697, 298],[713, 323],[729, 350],[745, 377],[760, 404],[776, 429],[785, 446],[783, 454],[760, 455],[730, 455],[699, 455],[686, 449],[674, 429],[662, 407],[651, 388],[638, 365],[621, 338],[614, 337],[598, 361],[596, 373],[596, 401],[596, 422],[596, 449],[592, 455],[568, 455],[541, 455],[514, 455],[507, 451],[506, 425],[506, 399]
-
-Y outer
-[637, 363],[637, 335],[637, 313],[636, 304],[627, 285],[617, 264],[606, 241],[596, 218],[584, 194],[574, 171],[563, 148],[552, 125],[539, 94],[542, 88],[572, 87],[603, 87],[629, 88],[636, 95],[644, 114],[654, 140],[664, 165],[672, 186],[679, 202],[685, 202],[692, 186],[701, 164],[710, 142],[720, 117],[729, 94],[736, 88],[761, 87],[790, 87],[820, 88],[825, 94],[817, 116],[807, 136],[797, 158],[786, 181],[776, 203],[765, 226],[754, 249],[743, 274],[729, 302],[727, 311],[727, 334],[727, 361],[727, 390],[727, 415],[723, 421],[697, 422],[673, 422],[642, 421],[637, 415],[637, 389]
-
-O outer
-[548, 253],[548, 229],[548, 203],[552, 177],[560, 153],[574, 130],[591, 112],[614, 98],[637, 89],[664, 84],[691, 83],[718, 87],[744, 95],[764, 105],[784, 122],[801, 145],[810, 165],[816, 194],[817, 221],[817, 246],[817, 272],[817, 300],[815, 328],[808, 352],[797, 373],[783, 391],[763, 407],[741, 418],[716, 426],[692, 428],[664, 428],[637, 423],[613, 413],[590, 398],[573, 380],[561, 361],[552, 336],[548, 305],[548, 279]
-
-O inner
-[638, 257],[638, 235],[639, 210],[644, 184],[661, 166],[685, 161],[709, 169],[722, 186],[726, 211],[727, 233],[727, 258],[727, 283],[726, 309],[719, 333],[703, 347],[685, 351],[662, 347],[646, 332],[640, 311],[638, 285]
-
-*/
